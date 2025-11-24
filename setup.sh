a7b4be68c905a1bf011f713a11bbaf10af5e7b03
Jannik Lindemann <jannik.lindemann3@gmail.com>
2025-11-24 16:28:57 +0100

    Modify setup.sh

--- setup.sh
+++ setup.sh
@@ -20,3 +20,18 @@ for id in {1..3}; do
   output=$("${cmd[@]}")
   printf '%s\n' "$output"
 done
+
+for id in {1..3}; do
+  # build cmd
+  cmd=(
+    "${SYSDS_CMD_COMMON[@]}"
+    "$SYSDS_JAR_CP"          # or OOC, doesn’t matter for datagen
+    -f "$FILE"
+    -exec singlenode
+    -args 60000 1000 1.0 "$id"
+  )
+
+  printf 'SETUP CMD: %q ' "${cmd[@]}"; echo
+  output=$("${cmd[@]}")
+  printf '%s\n' "$output"
+done

ea0cf18199eac8b0ccaa4c9a740bb8a91b70fc8b
Jannik Lindemann <jannik.lindemann3@gmail.com>
2026-01-13 17:25:35 +0100

    More experiments

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ for id in {1..3}; do
   printf '%s\n' "$output"
 done
 
-for id in {1..1}; do
+for id in {1..2}; do
   # build cmd
   cmd=(
     "${SYSDS_CMD_COMMON[@]}"
@@ -29,6 +29,21 @@ for id in {1..1}; do
     -f "$FILE"
     -exec singlenode
     -args 1000000 1000 1.0 "$id"
+  )
+
+  printf 'SETUP CMD: %q ' "${cmd[@]}"; echo
+  output=$("${cmd[@]}")
+  printf '%s\n' "$output"
+done
+
+for id in {1..2}; do
+  # build cmd
+  cmd=(
+    "${SYSDS_CMD_COMMON[@]}"
+    "$SYSDS_JAR_CP"          # or OOC, doesn’t matter for datagen
+    -f "$FILE"
+    -exec singlenode
+    -args 1000000 1 1.0 "$id"
   )
 
   printf 'SETUP CMD: %q ' "${cmd[@]}"; echo
